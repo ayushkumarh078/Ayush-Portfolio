dfc8a83a64336fe79baa0aa102517602afe9eb0c
ayushkumarh078 <ayushkumarh078@gmail.com>
2026-07-08 23:25:36 +0530

    Fix GitHub pages deployment via GitHub Actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/Ayush-Portfolio",
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
